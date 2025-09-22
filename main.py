4320cba930be84a8e4f3e96f27f15b436314e635
Jan-Kudela <jochanan.jorgen@gmail.com>
2025-09-22 22:01:41 +0200

    Zapsána funkce pro vytvoření databáze a pro připojení k této databázi

--- main.py
+++ main.py
@@ -3,3 +3,52 @@
 #vytvoreni_tabulky()
 
 #hlavni_menu()
+
+import mysql.connector
+
+DB_NAME = "manager_library"
+PASSWORD = "19791979"
+
+def create_database(cursor, db_name):
+    cursor.execute(f"CREATE DATABASE IF NOT EXISTS {db_name}")
+
+def connect_to_db():
+    try:
+        conn = mysql.connector.connect(
+            host = "localhost",
+            user = "root",
+            password = PASSWORD,
+            )
+        cursor = conn.cursor()
+        create_database(cursor, DB_NAME)
+        cursor.close()
+        conn.close()
+
+        conn = mysql.connector.connect(
+            host = "localhost",
+            user = "root",
+            password = PASSWORD,
+            database = DB_NAME
+            )
+        print(f"Připojeno k databázi {DB_NAME}.")
+        return conn
+
+    except mysql.connector.Error as err:
+        print (f"Chyba při připojování: {err}")
+        return None
+
+    
+   
+
+def main():
+
+    conn = connect_to_db()
+    if conn:
+        cursor = conn.cursor()
+
+        
+ 
+
+
+if __name__ == "__main__":
+    main()
