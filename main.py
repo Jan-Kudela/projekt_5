26151b814545992dc20aa69b4f816e371c602a2a
Jan-Kudela <jochanan.jorgen@gmail.com>
2025-10-06 21:23:39 +0200

    opravena funkce zobrazit_ukoly

--- main.py
+++ main.py
@@ -5,20 +5,21 @@
 #hlavni_menu()
 
 import mysql.connector
-from datetime import date
+
 
 DB_NAME = "manager_library"
 PASSWORD = "19791979"
 
+
 def create_database(cursor, db_name):
-    cursor.execute(f"CREATE DATABASE IF NOT EXISTS {db_name}")
+    cursor.execute(f"CREATE DATABASE IF NOT EXISTS {db_name};")
 
 def connect_to_db():
     try:
         conn = mysql.connector.connect(
             host = "localhost",
             user = "root",
-            password = PASSWORD,
+            password = PASSWORD
             )
         cursor = conn.cursor()
         create_database(cursor, DB_NAME)
@@ -35,7 +36,7 @@ def connect_to_db():
         return conn
 
     except mysql.connector.Error as err:
-        print (f"Chyba při připojování: {err}")
+        print (f"Chyba při připojování: {err}.")
         return None
 
     
@@ -48,8 +49,8 @@ def create_table(conn):
                 ID INT PRIMARY KEY AUTO_INCREMENT,
                 Nazev VARCHAR(100) NOT NULL,
                 Popis VARCHAR (255) NOT NULL,
-                Stav VARCHAR (20) Nezahájeno,
-                Datum vytvoreni DATE);""")
+                Stav VARCHAR (20) DEFAULT 'Nezahajeno',
+                Datum_vytvoreni DATE DEFAULT (CURDATE()));""")
         
         conn.commit()
     finally:
@@ -81,7 +82,8 @@ def zobrazit_ukoly(conn):
     try:
         cursor = conn.cursor()
         cursor.execute("SELECT * FROM ukoly")
-        seznam =  cursor.fetchall()
+        seznam = cursor.fetchall()
+        
         return seznam
 
     except mysql.connector.Error as err:
@@ -97,55 +99,60 @@ def main():
         cursor = conn.cursor()
         create_table(conn)
 
-    while True:
-        print("Správce úkolů - Hlavní menu\n")
-        print("1. Přidat úkol")
-        print("2. Zobrazit úkoly")
-        print("3. Aktualizovat úkol")
-        print("4. Odstranit úkol")
-        print("5. Ukončit program")
-
         while True:
-                choice_number = (input("Vyberte možnost (1-5):"))
-                choice_nr_checked = digit_check(choice_number)
-            
-                if choice_nr_checked not in range(1,5):
-                    print("Zadané číslo musí být v rozsahu 1-5")
-                else:
-                    break
+            print("Správce úkolů - Hlavní menu\n")
+            print("1. Přidat úkol")
+            print("2. Zobrazit úkoly")
+            print("3. Aktualizovat úkol")
+            print("4. Odstranit úkol")
+            print("5. Ukončit program")
 
-            
-        if choice_nr_checked == 1:
             while True:
-                task_name = input("Zadejte název úkolu:")
-                task_cont = input("Zadejte popis úkolu:")
-                if not task_name or not task_cont:
-                    print("Název úkolu i jeho popis musí být vyplněny.")
-                else:
-                    pridat_ukol(task_name, task_cont)
-                    break
+                    choice_number = (input("Vyberte možnost (1-5):"))
+                    choice_nr_checked = digit_check(choice_number)
+                
+                    if choice_nr_checked not in range(1,6):
+                        print("Zadané číslo musí být v rozsahu 1-5")
+                    else:
+                        break
 
-        
-        elif choice_nr_checked == 2:
-            seznam = zobrazit_ukoly()
-            if not seznam:
-                print("žádný úkol není zadán\n")
-            else:    
-                print(seznam)
-
-        
-        elif choice_nr_checked == 3:
-            if not seznam:
-                print("žádný úkol není zadán\n")
-            else:
-                print(seznam)
+                
+            if choice_nr_checked == 1:
+                while True:
+                    task_name = input("Zadejte název úkolu:")
+                    task_cont = input("Zadejte popis úkolu:")
+                    if not task_name or not task_cont:
+                        print("Název úkolu i jeho popis musí být vyplněny.")
+                    else:
+                        pridat_ukol(conn,task_name, task_cont)
+                        print(f"Úkol {task_name} byl úspěšně zadán.")
+                        break
 
             
+            elif choice_nr_checked == 2:
+                seznam = zobrazit_ukoly(conn)
+                if not seznam:
+                    print("žádný úkol není zadán\n")
+                else:    
+                    for line in seznam:
+                        print(
+                    f"{line[0]}. {line[1]} - {line[2]} - {line[3]} - {line[4]}"
+                    )
 
-        else:
-            print("Program je ukončen.")
-            break
+            
+            elif choice_nr_checked == 3:
+                if not seznam:
+                    print("žádný úkol není zadán\n")
+                else:
+                    print(seznam)
 
+                
+
+            else:
+                print("Program je ukončen.")
+                break
+    else:
+        print("Nelze se připojit k databázi.")
 
 if __name__ == "__main__":
     main()
